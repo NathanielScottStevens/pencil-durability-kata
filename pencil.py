65bd8821a0b1854a12a7bad816e24a54eed13eab
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2018-04-25 21:40:27 -0400

    Lowercase letters degrade the point.

--- pencil.py
+++ pencil.py
@@ -1,10 +1,17 @@
 class Pencil:
 
-    def __init__(self):
+    def __init__(self, durability=100):
         self.paper = ""
+        self.durability = durability
+        self.point = durability
 
     def write(self, text):
-        self.paper += text
+        for c in text:
+            if self.point > 0:
+                self.paper += c
+                self.point -= 1
+            else:
+                self.paper += ' '
 
     def read(self):
         return self.paper
--- pencil_test.py
+++ pencil_test.py
@@ -4,7 +4,7 @@ from pencil import Pencil
 
 class PencilTest(unittest.TestCase):
 
-    def test_writes_text(self):
+    def test_should_write_text(self):
         pencil = Pencil()
 
         text = "What possessed the first man to ride a horse?"
@@ -12,4 +12,26 @@ class PencilTest(unittest.TestCase):
 
         self.assertEqual(text, pencil.read())
 
+    def test_when_pencil_writes_additional_text_it_should_append_to_existing_text(self):
+        pencil = Pencil()
+
+        text_part_a = "I'm not the kind of man "
+        text_part_b = "who can just walk into a bouncy castle."
+
+        pencil.write(text_part_a)
+        pencil.write(text_part_b)
+
+        expected = text_part_a + text_part_b
+
+        self.assertEqual(expected, pencil.read())
+
+    def test_when_pencil_is_completely_dull_it_should_write_spaces(self):
+        pencil = Pencil(durability=0)
+        pencil.write("text")
+        self.assertEqual("    ", pencil.read())
+
+    def test_when_pencil_writes_lowercase_letters_it_should_degrade_by_one(self):
+        pencil = Pencil(durability=2)
+        pencil.write("text")
+        self.assertEqual("te  ", pencil.read())
 
